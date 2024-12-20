4186c0deb4bb0188a21a4b7b2790e290f24382e5
wdurrett115 <durrettwilliam54@gmail.com>
2024-12-20 15:24:49 -0600

    rudimentary server set up

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+
+func routes() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /{$}", home)
+
+	return mux
+}
+
 func main() {
-	fmt.Println("Start")
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	srv := &http.Server{
+		Addr:         *addr,
+		Handler:      routes(),
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	logger.Info("starting server", "addr", *addr)
+
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
 }
